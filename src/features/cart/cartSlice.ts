ab8d9c838e0e5d8d682c12696a3f29e73e1ae995
Sebaspa <sebaspa0107@gmail.com>
2023-03-09 11:36:03 -0500

    Crear funcion para agregar un producto al carrito

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,12 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { type TCartItem } from '../../types/product'
+
+const cart: TCartItem[] = []
 const initialState = {
-  cart: []
+  cart
 }
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
-  reducers: {}
+  reducers: {
+    addToCart: (state, action) => {
+      const itemInCart = state.cart.find(
+        (item: TCartItem) => item.id === action.payload.id
+      )
+      if (itemInCart !== null && itemInCart !== undefined) {
+        itemInCart.quantity++
+      } else {
+        state.cart.push({ ...action.payload, quantity: 1 })
+      }
+    }
+  }
 })
 
 export default cartSlice.reducer
+
+export const { addToCart } = cartSlice.actions
