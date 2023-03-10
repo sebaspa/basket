d1b0c7972192e2a3cc60a537854b12ff9315a5b0
Sebaspa <sebaspa0107@gmail.com>
2023-03-10 17:00:15 -0500

    Filtro Busqueda del producto por su nombre

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,5 +1,45 @@
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Products } from '../components'
+import { type RootState } from '../store'
+
 export const SearchPage = (): JSX.Element => {
+  const { products } = useSelector((store: RootState) => store.product)
+  const [productsToFilter, setProductsToFilter] = useState(products)
+
+  const filterProductsByName = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    const searchText = e.target.value
+    if (searchText === '') {
+      setProductsToFilter(products)
+    } else {
+      const filteredProducts = products.filter((product) =>
+        product.title.toLowerCase().includes(searchText.toLowerCase())
+      )
+      setProductsToFilter(filteredProducts)
+    }
+  }
+
   return (
-    <div>SearchPage</div>
+    <>
+      <div className="container mx-auto px-4">
+        <h2 className="text-xl md:text-2xl font-bold text-center my-8">
+          Search Page
+        </h2>
+        <div className="mt-6 mb-8">
+          <label htmlFor="search">Search by product name</label>
+          <input
+            id="search"
+            type="search"
+            className="w-full border border-gray-500 rounded-md h-9 px-3"
+            onChange={(e) => {
+              filterProductsByName(e)
+            }}
+          />
+        </div>
+        <Products isLoading={false} products={productsToFilter} />
+      </div>
+    </>
   )
 }
